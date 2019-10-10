9cccff873a1b51f622e9d99f0494237a8e6da0d3
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-10-10 15:26:27 +0800

    Solve exercise 1.2.14 - EvenlyDivide.java

---
+++ chapter-1-elements-of-programming/EvenlyDivide.java
@@ -0,0 +1,15 @@
+/* *****************************************************************************
+ *  Name:              Ryan Ben S. Villanueva
+ *  1.2.14 Write a program that takes two positive integers as command-line
+ *  arguments and prints true if either evenly divides the other.
+ *  Last modified:     October 10, 2019
+ **************************************************************************** */
+
+public class EvenlyDivide {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        boolean evenlyDivides = ((a % b == 0) || (b % a == 0));
+        System.out.println(evenlyDivides);
+    }
+}
