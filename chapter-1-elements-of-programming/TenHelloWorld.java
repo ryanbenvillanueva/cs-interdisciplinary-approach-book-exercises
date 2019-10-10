0fe2b97ca28e2c2838da045cf53f021778460fed
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-10-10 13:02:45 +0800

    Solve exercise 1.1.1 - TenHelloWorld.java

---
+++ chapter-1-elements-of-programming/TenHelloWorld.java
@@ -0,0 +1,20 @@
+/* *****************************************************************************
+ *  Name:              Ryan Ben S. Villanueva
+ *  1.1.1 Write a program that prints the Hello, World message 10 times.
+ *  Last modified:     October 10, 2019
+ **************************************************************************** */
+
+public class TenHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+}
