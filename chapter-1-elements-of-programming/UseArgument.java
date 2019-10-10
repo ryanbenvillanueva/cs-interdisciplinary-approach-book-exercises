4030ab8c438af1ba177e8229c17146bb39b49d7b
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-10-10 13:32:24 +0800

    Solve exercise 1.1.6 - UseArgument.java

---
+++ chapter-1-elements-of-programming/UseArgument.java
@@ -0,0 +1,31 @@
+/* *****************************************************************************
+ *  Name:              Ryan Ben S. Villanueva
+ *  1.1.6 Modify UseArgument.java to make a program UseThree.java that takes
+ *  three names as command-line arguments and prints a proper sentence with the
+ *  names in the reverse of the order given, so that, for example, java UseThree Alice
+ *  Bob Carol prints Hi Carol, Bob, and Alice.
+ *  Last modified:     October 10, 2019
+ **************************************************************************** */
+
+/******************************************************************************
+ *  Compilation:  javac UseArgument.java
+ *  Execution:    java UseArgument yourname
+ *
+ *  Prints "Hi, Bob. How are you?" where "Bob" is replaced by the
+ *  command-line argument.
+ *
+ *  % java UseArgument Bob
+ *  Hi, Bob. How are you?
+ *
+ *  % java UseArgument Alice
+ *  Hi, Alice. How are you?
+ *
+ ******************************************************************************/
+
+public class UseArgument {
+
+    public static void main(String[] args) {
+        System.out.println("Hi " + args[2] + ", " + args[1] + ", and " + args[0] + ".");
+    }
+
+}
