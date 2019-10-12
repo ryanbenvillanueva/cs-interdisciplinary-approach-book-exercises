d3e94e5cec218260dad2e3605381f75c92a7be95
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-10-12 21:18:19 +0800

    Solve exercise 1.2.21 - SumOfSines.java

---
+++ chapter-1-elements-of-programming/SumOfSines.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name:              Ryan Ben S. Villanueva
+ *  1.2.21 Write a program that takes a double command-line argument t and
+ *  prints the value of sin(2t)  sin(3t).
+ *  Last modified:     October 12, 2019
+ **************************************************************************** */
+
+public class SumOfSines {
+    public static void main(String[] args) {
+        double t = Double.parseDouble(args[0]);
+        double value = Math.sin(2 * t) + Math.sin(3 * t);
+        System.out.println(value);
+    }
+}
