80c241a79db5e1cd30c46da25a875d368c076935
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-10-12 19:45:12 +0800

    Solve exercise 1.2.18 - EuclideanDistance.java

---
+++ chapter-1-elements-of-programming/EuclideanDistance.java
@@ -0,0 +1,16 @@
+/* *****************************************************************************
+ *  Name:              Ryan Ben S. Villanueva
+ *  1.2.18 Write a program that takes two integer command-line arguments x and
+ *  y and prints the Euclidean distance from the point (x, y) to the origin
+ *  (0, 0).
+ *  Last modified:     October 12, 2019
+ **************************************************************************** */
+
+public class EuclideanDistance {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        double distance = Math.sqrt((y * y) + (x * x));
+        System.out.println(distance);
+    }
+}
--- chapter-1-elements-of-programming/EvenlyDivide.java
+++ chapter-1-elements-of-programming/EvenlyDivide.java
@@ -10,6 +10,7 @@ public class EvenlyDivide {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         boolean evenlyDivides = ((a % b == 0) || (b % a == 0));
+        
         System.out.println(evenlyDivides);
     }
 }
