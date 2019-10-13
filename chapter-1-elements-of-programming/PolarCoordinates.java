ae7ff1dee96e3403efd8df90e0c436c0c0de0aca
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-10-13 15:49:46 +0800

    Solve exercise 1.2.26 - PolarCoordinates.java

---
+++ chapter-1-elements-of-programming/PolarCoordinates.java
@@ -0,0 +1,22 @@
+/* *****************************************************************************
+ *  Name:              Ryan Ben S. Villanueva
+ *  1.2.26 Polar coordinates. Write a program that converts from Cartesian
+ *  to polar coordinates. Your program should accept two double commandline
+ *  arguments x and y and print the polar coordinates r and . Use the
+ *  method Math.atan2(y, x) to compute the arctangent value of y/x that is
+ *  in the range from -pi to pi.
+ *  Last modified:     October 13, 2019
+ **************************************************************************** */
+
+public class PolarCoordinates {
+    public static void main(String[] args) {
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+        double theta = Math.toDegrees(Math.atan2(y, x));
+        double r = Math.sqrt((x * x) + (y * y));
+
+        System.out.println("The point (" + x + "," + y + ") is "
+                                   + "(" + r + "," + theta + ") "
+                                   + "in Polar Coordinates.");
+    }
+}
